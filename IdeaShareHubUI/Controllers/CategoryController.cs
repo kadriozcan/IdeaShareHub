b8be061ff6ce274dd45d7a1330c8b163999553df
kadriozcan <kadri.ozcan@outlook.com>
2023-08-23 01:29:12 +0300

    started CategoryController and its view

---
+++ IdeaShareHubUI/Controllers/CategoryController.cs
@@ -0,0 +1,28 @@
+﻿using Business.Concrete;
+using Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IdeaShareHub.Controllers
+{
+    public class CategoryController : Controller
+    {
+
+        CategoryManager categoryManager = new CategoryManager();
+
+        // GET: Category
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetAll()
+        {
+            List<Category> categories = categoryManager.GetAll();
+            return View(categories);
+        }
+    }
+}
